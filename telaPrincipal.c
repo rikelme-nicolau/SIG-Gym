14d5134d0813851226a6b595b271c414a5a596de
Flávia Carvalho <flavia.carvalho.124@ufrn.edu.br>
2025-08-15 18:00:53 -0300

    ADD: tela aluno

--- telaPrincipal.c
+++ telaPrincipal.c
@@ -1,3 +1,6 @@
+#include <stdio.h>
+#include <stdlib.h>
+
 char telaPrincipal(void) {
 
     char op;
@@ -17,3 +20,26 @@ char telaPrincipal(void) {
 
     return op;
     }
+
+    
+    char moduloAluno() {
+
+    printf("=================================\n");
+    printf("|         MÓDULO ALUNO          |\n");
+    printf("=================================\n");
+    printf("| 1. Cadastrar Novo Aluno       |\n");
+    printf("| 2. Atualizar Dados do Aluno   |\n");
+    printf("| 3. Visualizar Dados do Aluno  |\n");
+    printf("| 4. Deletar Aluno              |\n");
+    printf("| 0. Sair                       |\n");
+    printf("=================================\n");
+
+    char op_aluno;
+    printf("O que deseja fazer? Escolha uma das opções acima: ");
+    scanf(" %c", &op_aluno); 
+
+    return op_aluno;
+}
+
+
+
