6ca39a88200321d1a068431d883e7cfe3a11263e
Rikelme Nicolau <contato.rikelmenicolau@gmail.com>
2025-08-15 15:13:22 -0300

    feat(telaPrincipal): adicionar módulo telaPrincipal.c

--- sig-gym.c
+++ sig-gym.c
@@ -8,26 +8,13 @@
 ///////////////////////////////////////////////////////////////////////////////
 
 #include <stdio.h>
+#include "telaPrincipal.c"
 
 int main(void) {
     char opcao;
 
     do {
         opcao = telaPrincipal();
-        switch(opcao) {
-            case '1':   moduloAluno();
-                break;
-            case '2':   moduloPlanos();
-                break;
-            case '3':   moduloMatricula();
-                break;
-            case '4':   moduloFluxo();
-                break;
-            case '5':   moduloRH();
-                break;
-            case '6':   telaSobre();
-                break;
-        }
     } while (opcao != '0');
 
     return 0;
