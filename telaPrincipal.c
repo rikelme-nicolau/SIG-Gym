9499e9f396f802de0772e0f12d4d09f40953fc0e
Flávia Carvalho <flavia.carvalho.124@ufrn.edu.br>
2025-08-15 18:07:04 -0300

    ADD: tela dos planos

--- telaPrincipal.c
+++ telaPrincipal.c
@@ -42,4 +42,23 @@ char telaPrincipal(void) {
 }
 
 
+char moduloPlanos() {
+
+    printf("=================================\n");
+    printf("|         MÓDULO PLANOS         |\n");
+    printf("=================================\n");
+    printf("| 1. Cadastrar Novo Plano       |\n");
+    printf("| 2. Atualizar Plano            |\n");
+    printf("| 3. Visualizar Planos          |\n");
+    printf("| 4. Deletar Plano              |\n");
+    printf("| 0. Sair                       |\n");
+    printf("=================================\n");
+
+    char op_plano;
+    printf("O que deseja fazer? Escolha uma das opções acima: ");
+    scanf(" %c", &op_plano); 
+
+    return op_plano;
+}
+
 
