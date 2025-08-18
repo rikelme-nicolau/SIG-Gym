1ecccd8f754c3a467c9620e77fc387d1c0abff1c
Rikelme Nicolau <contato.rikelmenicolau@gmail.com>
2025-08-18 10:40:56 -0300

    feat: add header.h

--- src/ui/telaPlano.c
+++ src/ui/telaPlano.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
+#include "header.h"
 
-char moduloPlanos(void) {
+char telaPlano(void) {
 
 char op;
 
--- src/ui/telaPrincipal.c
+++ src/ui/telaPrincipal.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "header.h"
 
 char telaPrincipal(void) {
 
