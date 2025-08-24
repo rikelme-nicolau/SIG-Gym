76c0a67a7c0cc9e5e5a750113841c4c6c5964835
Flavia <flavia.carvalho.124@ufrn.edu.br>
2025-08-24 06:24:15 -0300

    feat: add tela excluir plano.

---
+++ src/ui/plano/excluirPlano.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "header_tela.h"
+
+char telaExcluirPlano(void) {
+
+    char op;    
+
+    printf("\n");
+    printf("=================\n");
+    printf(" EXCLUIR PLANO\n");
+    printf("=================\n");
+    printf("  ...EM BREVE... \n");
+    printf("\n");
+    printf("<Aperte 0 para sair!>\n");
+    scanf(" %c", &op);
+
+    return op;
+}
