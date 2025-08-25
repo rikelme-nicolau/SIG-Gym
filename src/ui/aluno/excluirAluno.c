5c3191d87f6fe25f054ea799f3a25ce3679373e3
Rikelme Nicolau <contato.rikelmenicolau@gmail.com>
2025-08-25 19:36:47 -0300

    feat: add navegação do módulo aluno/atualizar + aluno/excluir

--- src/ui/aluno/excluirAluno.c
+++ src/ui/aluno/excluirAluno.c
@@ -9,7 +9,17 @@ char telaExcluirAluno(void) {
     printf("=================\n");
     printf(" EXCLUIR ALUNO\n");
     printf("=================\n");
-    printf("  ...EM BREVE... \n");
+    printf("Ids:\n");
+    printf("001\n");
+    printf("002\n");
+    printf("003\n");
+    printf("004\n");
+    printf("\n");
+    printf("Digite o id do aluno que deseja excluir:\n");
+    printf("\n");
+    getchar();
+    printf("<pressione enter para simular!>\n");
+    getchar();
     printf("\n");
     printf("<Aperte 0 para sair!>\n");
     scanf(" %c", &op);
