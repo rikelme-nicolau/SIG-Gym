27a5be28d7eab08ce6bde6847e8190d32248dbb6
Flavia <flavia.carvalho.124@ufrn.edu.br>
2025-08-27 11:14:36 -0300

    feat: header par add cores.

---
+++ src/cores.h
@@ -0,0 +1,17 @@
+#ifndef COLORS_H
+#define COLORS_H
+//CORES🔱
+#define RESET   "\033[0m"
+#define RED     "\033[1;31m"
+#define GREEN   "\033[1;32m"
+#define YELLOW  "\033[1;33m"
+#define BLUE    "\033[1;34m"
+#define MAGENTA "\033[1;35m"
+#define CYAN    "\033[1;36m"
+#define WHITE   "\033[1;37m"
+//ESTILO DE TEXTO
+#define NEGRITO    "\033[1m"
+#define ITALICO  "\033[3m"
+#define SUBLINHADO   "\033[4m"
+
+#endif
