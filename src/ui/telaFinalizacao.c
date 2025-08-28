6685ffaf67928ec334e2fe6eb887033c4a2d3c14
Flavia <flavia.carvalho.124@ufrn.edu.br>
2025-08-28 10:40:06 -0300

    Style: melhorando o visual.

--- src/ui/telaFinalizacao.c
+++ src/ui/telaFinalizacao.c
@@ -1,19 +1,26 @@
 #include <stdio.h>
+#include "cores.h"
 
 void telaFinalizacao(void){
-    printf("_____________________________________________\n");
-    printf("               FIM DO PROGRAMA               \n");
-    printf("_____________________________________________\n");
-    printf("      Obrigado por utilizar o SIG-Gym!       \n");
-    printf("    O programa foi desenvolvido para fins    \n");
-    printf("       de aprendizado e não comercial.       \n");
-    printf("                 OBJETIVO:                   \n");
-    printf("     Obtenção de nota da disciplina de       \n");
-    printf("           DCT1106 -- Programação            \n");
-    printf("   Ministrada pelo professor e orientador    \n");
-    printf("              Flavius Gorgônio               \n");
-    printf("     Centro de Ensino Superior do Seridó     \n");
-    printf("   Departamento de Computação e Tecnologia   \n");
+    printf("╔═══════════════════════════════════════════════╗\n");
+    printf("║                FIM DO PROGRAMA                ║\n");
+    printf("╠═══════════════════════════════════════════════╣\n");
+    printf("║       Obrigado por utilizar o SIG-Gym!        ║\n");
+    printf("║     O programa foi desenvolvido para fins     ║\n");
+    printf("║     de aprendizado e não fins comerciais.     ║\n");
+    printf("╠═══════════════════════════════════════════════╣\n");
+    printf("║                    OBJETIVO                   ║\n");
+    printf("╠═══════════════════════════════════════════════╣\n");
+    printf("║      Obtenção de nota da disciplina de        ║\n");
+    printf("║           DCT1106 -- Programação.             ║\n");
+    printf("║     Ministrada pelo professor e orientador    ║\n");
+    printf("║               Flavius Gorgônio.               ║\n");
+    printf("║ Curso - Bacharelado em Sistemas da Informação ║\n");
+    printf("║      Centro de Ensino Superior do Seridó      ║\n");
+    printf("║    Departamento de Computação e Tecnologia    ║\n");
+    printf("╠═══════════════════════════════════════════════╣\n");
+    printf("║       Pressione |ENTER| para finalizar.       ║\n");
+    printf("╚═══════════════════════════════════════════════╝\n");
     printf("\n");
     getchar();
 
