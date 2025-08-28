b9dbf26de6a9deaf94d95c7c63477bd3a1d58b67
Flavia <flavia.carvalho.124@ufrn.edu.br>
2025-08-28 10:41:50 -0300

    Style: melhorando o visual.

--- src/ui/aluno/cadastrarAluno.c
+++ src/ui/aluno/cadastrarAluno.c
@@ -7,17 +7,16 @@ char telaCadastrarAluno() {
     char op;
 
     printf("\n");
-    printf(RED"________________________________________________\n"RESET);
-    printf(RED"|                                              |\n"RESET);
-    printf(RED"|"YELLOW"                CADASTRAR ALUNO               "RED"|\n"RESET);
-    printf(RED"|______________________________________________|\n"RESET);
-    printf(RED"|"YELLOW"   As informações necessárias para realizar   "RED"|\n"RESET);
-    printf(RED"|"YELLOW"      o cadastro estão listadas a abaixo:     "RED"|\n"RESET);
-    printf(RED"|----------------------------------------------|\n"RESET);
-    printf(RED"|"YELLOW" 1. NOME COMPLETO     "RED"|"YELLOW" 2. DATA DE NASCIMENTO "RED"|\n"RESET);
-    printf(RED"|"YELLOW" 3. CPF               "RED"|"YELLOW" 4. ENDEREÇO           "RED"|\n"RESET);
-    printf(RED"|"YELLOW" 5. TELEFONE          "RED"|"YELLOW" 6. EMAIL              "RED"|\n"RESET);
-    printf(RED"|______________________________________________|\n"RESET);
+    printf("╔══════════════════════════════════════════════╗\n");
+    printf("║                CADASTRAR ALUNO               ║\n");
+    printf("╠══════════════════════════════════════════════╣\n");
+    printf("║   As informações necessárias para realizar   ║\n");
+    printf("║      o cadastro estão listadas a abaixo:     ║\n");
+    printf("╠═════════════════════╦════════════════════════╣\n");
+    printf("║ 1. NOME COMPLETO    ║ 2. DATA DE NASCIMENTO  ║\n");
+    printf("║ 3. CPF              ║ 4. ENDEREÇO            ║\n");
+    printf("║ 5. TELEFONE         ║ 6. EMAIL               ║\n");
+    printf("╚═════════════════════╩════════════════════════╝\n");
     printf("\n");
     printf("<operação de cadastro ainda não está pronta, pressione 0 para sair!>\n");
 
