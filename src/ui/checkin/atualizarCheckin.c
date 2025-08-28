ce5182a309ccc263a17e4720745dfe4f93d89d88
Flavia <flavia.carvalho.124@ufrn.edu.br>
2025-08-28 10:42:32 -0300

    Style: melhorando o visual.

--- src/ui/checkin/atualizarCheckin.c
+++ src/ui/checkin/atualizarCheckin.c
@@ -1,19 +1,21 @@
 #include <stdio.h>
 #include "header_tela.h"
+#include "cores.h"
 
 char telaAtualizarCheckin(void) {
 
     char op;
 
     printf("\n");
-    printf("=================\n");
-    printf(" ATUALIZAR CHECKIN\n");
-    printf("=================\n");
+    printf("╔═══════════════════════════════════════════════╗\n");
+    printf("║               ATUALIZAR CHECKIN               ║\n");
+    printf("╠═══════════════════════════════════════════════╣\n");
     printf("  ...EM BREVE... \n");
     printf("\n");
     printf("<Aperte 0 para sair!>\n");
     
-    scanf(" %c", &op);
+    scanf("%c", &op);
+    getchar();
 
     return op;
 
