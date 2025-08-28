6bf0f818ee54e32d6e1f4fd5bb3f0cba74673ede
Flavia <flavia.carvalho.124@ufrn.edu.br>
2025-08-28 10:43:24 -0300

    Style: melhorando o visual.

--- src/ui/matricula/atualizarMatricula.c
+++ src/ui/matricula/atualizarMatricula.c
@@ -1,19 +1,21 @@
 #include <stdio.h>
 #include "header_tela.h"
+#include "cores.h"
 
 char telaAtualizarMatricula(void) {
 
     char op;
 
     printf("\n");
-    printf("=================\n");
-    printf(" ATUALIZAR MATRICULA\n");
-    printf("=================\n");
+    printf("╔═══════════════════════════════════════════════╗\n");
+    printf("║              ATUALIZAR MATRICULA              ║\n");
+    printf("╠═══════════════════════════════════════════════╣\n");
     printf("  ...EM BREVE... \n");
     printf("\n");
     printf("<Aperte 0 para sair!>\n");
     
-    scanf(" %c", &op);
+    scanf("%c", &op);
+    getchar();
 
     return op;
 
