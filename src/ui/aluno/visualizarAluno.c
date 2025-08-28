b41e0bef31d9a724a87d573e8784f5d9bf2532e6
Flavia <flavia.carvalho.124@ufrn.edu.br>
2025-08-28 10:42:15 -0300

    Style: melhorando o visual.

--- src/ui/aluno/visualizarAluno.c
+++ src/ui/aluno/visualizarAluno.c
@@ -6,22 +6,21 @@ char telaVisualizarAluno(void) {
 
     char op;
     printf("\n");
-    printf(RED"_________________________________________________\n"RESET);
-    printf(RED"|                                               |\n"RESET);
-    printf(RED"|"YELLOW"               VISUALIZAR ALUNO                "RED"|\n"RESET);
-    printf(RED"|_______________________________________________|\n"RESET);
-    printf(RED"|"YELLOW"  Listados abaixo estão os alunos atualmente   "RED"|\n"RESET);
-    printf(RED"|"YELLOW"             cadastrados e ativos:             "RED"|\n"RESET);
-    printf(RED"|-----------------------------------------------|\n"RESET);
-    printf(RED"|"YELLOW"  IDs:"RED"|"YELLOW" NOMES:                                 "RED"|\n"RESET);//posteriormente add função que realiza esses prints de forma automática com nomes realmente cadastrados
-    printf(RED"|"YELLOW"  001 "RED"|"YELLOW" Enzo Primeiro                          "RED"|\n"RESET);
-    printf(RED"|"YELLOW"  002 "RED"|"YELLOW" Valentina Primeira                     "RED"|\n"RESET);
-    printf(RED"|"YELLOW"  003 "RED"|"YELLOW" Enzo Segundo                           "RED"|\n"RESET);
-    printf(RED"|"YELLOW"  004 "RED"|"YELLOW" Valentina Segunda                      "RED"|\n"RESET);
-    printf(RED"|-----------------------------------------------|\n"RESET);
-    printf(RED"|"YELLOW"                   "SUBLINHADO"!ATENÇÃO!"RESET"                   "RED"|\n"RESET);
-    printf(RED"|"YELLOW"    A seguir digite [APENAS] o ID do aluno:    "RED"|\n"RESET);
-    printf(RED"|_______________________________________________|\n"RESET);
+    printf("╔═══════════════════════════════════════════════╗\n");
+    printf("║               VISUALIZAR ALUNO                ║\n");
+    printf("╠═══════════════════════════════════════════════╣\n");
+    printf("║  Listados abaixo estão os alunos atualmente   ║\n");
+    printf("║             cadastrados e ativos:             ║\n");
+    printf("╠══════╦════════════════════════════════════════╣\n");
+    printf("║  IDs:║ NOMES:                                 ║\n");//posteriormente add função que realiza esses prints de forma automática com nomes realmente cadastrados
+    printf("║  001 ║ Enzo Primeiro                          ║\n");
+    printf("║  002 ║ Valentina Primeira                     ║\n");
+    printf("║  003 ║ Enzo Segundo                           ║\n");
+    printf("║  004 ║ Valentina Segunda                      ║\n");
+    printf("╠══════╩════════════════════════════════════════╣\n");
+    printf("║                   "SUBLINHADO"!ATENÇÃO!"RESET"                   ║\n");
+    printf("║    A seguir digite [APENAS] o ID do aluno:    ║\n");
+    printf("╚═══════════════════════════════════════════════╝\n");
     printf("\n");
     printf("<operação de visualização ainda não está pronta, pressione 0 para sair!>\n");
     
