dc932bf305b0faadbaf1e2c4477b295b3d6cb9d6
Flavia <flavia.carvalho.124@ufrn.edu.br>
2025-08-28 10:45:11 -0300

    Style: melhorando o visual.

--- src/ui/plano/visualizarPlano.c
+++ src/ui/plano/visualizarPlano.c
@@ -1,28 +1,30 @@
 #include <stdio.h>
 #include "header_tela.h"
+#include "cores.h"
 
 char telaVisualizarPlano(void) {
 
     char op;
     printf("\n");
-    printf("=================\n");
-    printf(" VISUALIZAR PLANO\n");
-    printf("=================\n");
-    printf("Ids Planos:\n");
-    printf("001\n");
-    printf("002\n");
-    printf("003\n");
-    printf("004\n");
-    printf("\n");
-    printf("Digite o id do plano que deseja visualizar:\n");
-    printf("\n");
-    getchar();
-    printf("<pressione enter para simular!>\n");
-    getchar();
-    printf("\n");
+    printf("╔═══════════════════════════════════════════════╗\n");
+    printf("║                VISUALIZAR PLANO               ║\n");
+    printf("╠═══════════════════════════════════════════════╣\n");
+    printf("║  Listados abaixo estão os planos atualmente   ║\n");
+    printf("║             cadastrados e ativos:             ║\n");
+    printf("╠══════╦════════════════════════════════════════╣\n");
+    printf("║  IDs:║ PLANO:                                 ║\n");//posteriormente add função que realiza esses prints de forma automática com nomes realmente cadastrados
+    printf("║  001 ║ Semanal                                ║\n");
+    printf("║  002 ║ Mensal                                 ║\n");
+    printf("║  003 ║ Anual                                  ║\n");
+    printf("║  004 ║ Diário                                 ║\n");
+    printf("╠══════╩════════════════════════════════════════╣\n");
+    printf("║                   "SUBLINHADO"!ATENÇÃO!"RESET"                   ║\n");
+    printf("║    A seguir digite [APENAS] o ID do plano:    ║\n");
+    printf("╚═══════════════════════════════════════════════╝\n");
     printf("<Aperte 0 para sair!>\n");
     
-    scanf(" %c", &op);
+    scanf("%c", &op);
+    getchar();
 
     return op;
     }
