9b284df0ccea4987183ca1c0b3bac897bf324a0e
Rikelme Nicolau <contato.rikelmenicolau@gmail.com>
2025-09-01 13:56:55 -0300

    style: alteração na estética da função telaCadastrarCheckin

--- src/ui/checkin/cadastrarCheckin.c
+++ src/ui/checkin/cadastrarCheckin.c
@@ -1,14 +1,11 @@
 #include <stdio.h>
 
-#include "cores.h"
 char telaCadastrarCheckin(void) {
     
     char op;
-
-    printf("\n");
-    printf("╔═══════════════════════════════════════════════╗\n");
-    printf("║               CADASTRAR CHECKIN               ║\n");
-    printf("╠═══════════════════════════════════════════════╣\n");
+    printf("====================\n");
+    printf("CADASTRAR CHECKIN\n");
+    printf("====================\n");
     printf("  ...EM BREVE... \n");
     printf("\n");
     printf("<Aperte 0 para sair!>\n");
