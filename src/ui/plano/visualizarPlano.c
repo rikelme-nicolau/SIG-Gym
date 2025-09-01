504c9e5dd3da2856ae94b80d7429316ca76d1eb1
Rikelme Nicolau <contato.rikelmenicolau@gmail.com>
2025-09-01 15:36:16 -0300

    style: alteração na estética da função telaVisualizarPlano

--- src/ui/plano/visualizarPlano.c
+++ src/ui/plano/visualizarPlano.c
@@ -1,26 +1,12 @@
 #include <stdio.h>
 
-#include "cores.h"
-
 char telaVisualizarPlano(void) {
 
     char op;
-    printf("\n");
-    printf("╔═══════════════════════════════════════════════╗\n");
-    printf("║                VISUALIZAR PLANO               ║\n");
-    printf("╠═══════════════════════════════════════════════╣\n");
-    printf("║  Listados abaixo estão os planos atualmente   ║\n");
-    printf("║             cadastrados e ativos:             ║\n");
-    printf("╠══════╦════════════════════════════════════════╣\n");
-    printf("║  IDs:║ PLANO:                                 ║\n");//posteriormente add função que realiza esses prints de forma automática com nomes realmente cadastrados
-    printf("║  001 ║ Semanal                                ║\n");
-    printf("║  002 ║ Mensal                                 ║\n");
-    printf("║  003 ║ Anual                                  ║\n");
-    printf("║  004 ║ Diário                                 ║\n");
-    printf("╠══════╩════════════════════════════════════════╣\n");
-    printf("║                   "SUBLINHADO"!ATENÇÃO!"RESET"                   ║\n");
-    printf("║    A seguir digite [APENAS] o ID do plano:    ║\n");
-    printf("╚═══════════════════════════════════════════════╝\n");
+    
+    printf("====================\n");
+    printf("VISUALIZAR PLANO\n");
+    printf("====================\n");
     printf("<Aperte 0 para sair!>\n");
     
     scanf("%c", &op);
