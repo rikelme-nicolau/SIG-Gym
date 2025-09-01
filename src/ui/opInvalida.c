f7b22a494302fc2a8708fa394fab9dfb21ac645b
Rikelme Nicolau <contato.rikelmenicolau@gmail.com>
2025-09-01 15:38:40 -0300

    style: alteração na função opInvalida

--- src/ui/opInvalida.c
+++ src/ui/opInvalida.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 
 void opInvalida(void){
-    printf("\n");
-    printf("╔═══════════════════════════════════════════════╗\n");
-    printf("║                OPÇÃO INVÁLIDA                 ║\n");
-    printf("╠═══════════════════════════════════════════════╣\n");
-    printf("║ Ocorreu algum erro, a opção que você digitou  ║\n");
-    printf("║        é inválida, por favor pressione        ║\n");
-    printf("║             |ENTER| para retornar.            ║\n");
-    printf("╚═══════════════════════════════════════════════╝\n");
+    
+    printf("====================\n");
+    printf("OPÇÃO INVÁLIDA\n");
+    printf("====================\n");
+    printf("A opção que você digitou é inválida\n");
+    printf("< pressione <enter> >\n");
+   
     getchar();
 }
