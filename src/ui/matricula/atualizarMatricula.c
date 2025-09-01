ab4a9c22b68c6435625e9ef0e68b07c3fc2006c1
Rikelme Nicolau <contato.rikelmenicolau@gmail.com>
2025-09-01 15:19:57 -0300

    style: alteração na estética da função telaAtualizarMatricula

--- src/ui/matricula/atualizarMatricula.c
+++ src/ui/matricula/atualizarMatricula.c
@@ -1,15 +1,13 @@
 #include <stdio.h>
 
-#include "cores.h"
-
 char telaAtualizarMatricula(void) {
 
     char op;
 
-    printf("\n");
-    printf("╔═══════════════════════════════════════════════╗\n");
-    printf("║              ATUALIZAR MATRICULA              ║\n");
-    printf("╠═══════════════════════════════════════════════╣\n");
+
+    printf("====================\n");
+    printf("ATUALIZAR MATRICULA\n");
+    printf("====================\n");
     printf("  ...EM BREVE... \n");
     printf("\n");
     printf("<Aperte 0 para sair!>\n");
