159c235975ed8ff6f071d2ee6e3f6f8aa44e3254
Rikelme Nicolau <contato.rikelmenicolau@gmail.com>
2025-09-01 13:45:47 -0300

    style: alteração na estética da função telaCadastrarAluno

--- src/ui/aluno/cadastrarAluno.c
+++ src/ui/aluno/cadastrarAluno.c
@@ -7,18 +7,9 @@ char telaCadastrarAluno() {
     char op;
 
     printf("\n");
-    printf("╔══════════════════════════════════════════════╗\n");
-    printf("║                CADASTRAR ALUNO               ║\n");
-    printf("╠══════════════════════════════════════════════╣\n");
-    printf("║   As informações necessárias para realizar   ║\n");
-    printf("║      o cadastro estão listadas a abaixo:     ║\n");
-    printf("╠═════════════════════╦════════════════════════╣\n");
-    printf("║ 1. NOME COMPLETO    ║ 2. DATA DE NASCIMENTO  ║\n");
-    printf("║ 3. CPF              ║ 4. ENDEREÇO            ║\n");
-    printf("║ 5. TELEFONE         ║ 6. EMAIL               ║\n");
-    printf("╚═════════════════════╩════════════════════════╝\n");
-    printf("\n");
-    printf("<operação de cadastro ainda não está pronta, pressione 0 para sair!>\n");
+    printf("==================\n");
+    printf("CADASTRAR ALUNO\n");
+    printf("==================\n");
 
     scanf("%c", &op);
     getchar();
