7d1320bdad59cff57e8d79135bdb853764d11982
Rikelme Nicolau <contato.rikelmenicolau@gmail.com>
2025-09-01 13:51:19 -0300

    style: alteração na estética da função telaAluno

--- src/ui/aluno/telaAluno.c
+++ src/ui/aluno/telaAluno.c
@@ -8,20 +8,14 @@ char op;
 
     printf("\n");
     
-    printf("╔══════════════════════════════════════════════╗\n");
-    printf("║                     ALUNO                    ║\n");
-    printf("╠══════════════════════════════════════════════╣\n");
-    printf("║ 1. CADASTRAR                                 ║\n");
-    printf("║ 2. VISUALIZAR                                ║\n");
-    printf("║ 3. ATUALIZAR                                 ║\n");
-    printf("║ 4. EXCLUIR                                   ║\n");
-    printf("║ 0. SAIR                                      ║\n");
-    printf("╠══════════════════════════════════════════════╣\n");
-    printf("║                   "SUBLINHADO"!ATENÇÃO!"RESET"                  ║\n");
-    printf("║  A seguir digite [APENAS] o número da ação:  ║\n");
-    printf("╚══════════════════════════════════════════════╝\n");
-    printf("\n");
-
+    printf("====================\n");
+    printf("ALUNO\n");
+    printf("====================\n");
+    printf("(1) CADASTRAR                                 ║\n");
+    printf("(2) VISUALIZAR                                ║\n");
+    printf("(3) ATUALIZAR                                 ║\n");
+    printf("(4) EXCLUIR                                   \n");
+    printf("(0) SAIR\n");
     scanf("%c", &op);
     getchar();
     return op;
