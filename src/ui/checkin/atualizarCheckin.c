bce40105831afef10ef4d66a5a823a6bee921a93
Rikelme Nicolau <contato.rikelmenicolau@gmail.com>
2025-09-26 07:27:58 -0300

    style: alteração na ui telaAtualizarCheckin

--- src/ui/checkin/atualizarCheckin.c
+++ src/ui/checkin/atualizarCheckin.c
@@ -4,17 +4,25 @@
 char telaAtualizarCheckin(void) {
 
     char op;
-    printf("====================\n");
-    printf("ATUALIZAR CHECKIN \n");
-    printf("====================\n");
-    printf("  ...EM BREVE... \n");
+    
     printf("\n");
-    printf("<Aperte 0 para sair!>\n");
+    
+    printf("=========================================================================\n");
+    printf("===                        ATUALIZAR CHECKIN                          ===\n");
+    printf("=========================================================================\n");
+    printf("===                                                                   ===\n");
+    printf("===  [1]                                                              ===\n");
+    printf("===  [2]                                                              ===\n");
+    printf("===  [3]                                                              ===\n");
+    printf("===  [4]                                                              ===\n");
+    printf("===  [5]                                                              ===\n");
+    printf("===                                                                   ===\n");
+    printf("=========================================================================\n");
+    printf("=========================================================================\n");
     
     scanf("%c", &op);
     getchar();
 
     limparTela();
     return op;
-
     }
