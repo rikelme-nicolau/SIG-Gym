f215bd22db6e23d78b846fb1b38f6f170abe7b3d
Rikelme Nicolau <contato.rikelmenicolau@gmail.com>
2025-09-27 11:48:52 -0300

    feat: add módulo RH

---
+++ src/ui/rh/telaRH.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "limparTela.h"
+
+char telaPlano(void) {
+
+char op;
+
+    printf("\n");
+    
+    printf("=========================================================================\n");
+    printf("===                              RH                                   ===\n");
+    printf("=========================================================================\n");
+    printf("===                                                                   ===\n");
+    printf("===  [1]  CADASTRAR                                                   ===\n");
+    printf("===  [2]  VISUALIZAR                                                  ===\n");
+    printf("===  [3]  ATUALIZAR                                                   ===\n");
+    printf("===  [4]  EXCLUIR                                                     ===\n");
+    printf("===  [0]  SAIR                                                        ===\n");
+    printf("===                                                                   ===\n");
+    printf("=========================================================================\n");
+    printf("=========================================================================\n");
+
+    scanf("%c", &op);
+    getchar();
+
+    limparTela();
+    return op;
+}
