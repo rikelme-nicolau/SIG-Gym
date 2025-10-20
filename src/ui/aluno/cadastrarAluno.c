c95eb1c1a578cdcd202a52d155a875e5ac6cb141
Rikelme Nicolau <contato.rikelmenicolau@gmail.com>
2025-10-20 18:14:35 -0300

    fix: correcao de bug na compilacao

--- src/ui/aluno/cadastrarAluno.c
+++ src/ui/aluno/cadastrarAluno.c
@@ -6,7 +6,7 @@
 #include "src/ui/plano/cadastrarPlano.h"
 #include "../utils/gerarMatricula.h"
 #include "src/ui/utils/validarNome.h"
-#include "src/ui/utils/valdiarCPF.h"
+#include "src/ui/utils/validarCPF.h"
 #include "src/ui/utils/validarNascimento.h"
 #include "src/ui/utils/validarTelefone.h"
 #include "src/ui/utils/validarEndereco.h"
