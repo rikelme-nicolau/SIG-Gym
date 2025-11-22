210e1e6e8f5aeaea3a62f4c4f71931816ae88ca4
Rikelme Nicolau <contato.rikelmenicolau@Gmail.com>
2025-11-22 18:09:28 -0300

    Add Brazilian name generator utilities

---
+++ src/ui/utils/geradorNomes.h
@@ -0,0 +1,9 @@
+#ifndef GERADOR_NOMES_H
+#define GERADOR_NOMES_H
+
+#include <stddef.h>
+
+void gerarNomeAleatorio(char *destino, size_t tamanho);
+void gerarNomeCompletoAleatorio(char *destino, size_t tamanho);
+
+#endif
